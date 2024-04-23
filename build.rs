e31192dde2d00d098c07e088953d4270d7902d3a
Christoffer Hald Christensen <chrishald@duck.com>
2024-04-23 18:55:16 +0200

    Made checkers board with clickable tiles

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
-fn main() {
-    slint_build::compile("ui/game_window.slint").expect("This file does exist");
+
+fn main(){
+    slint_build::compile("ui/game_window.slint").unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let game = GameWindow::new()?;
 
+    game.on_clicked(|index: i32| {
+        std::println!("Number {}", index);
+    });
+
     game.run()
 }
