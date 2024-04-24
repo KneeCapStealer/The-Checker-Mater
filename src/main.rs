79d4a845f8c70a7ea91db541f43858047400c32b
Christoffer Hald Christensen <chrishald@duck.com>
2024-04-24 18:07:56 +0200

    Modifying piece data in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use slint::Model;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let game = GameWindow::new()?;
+    let pieces = game.get_pieces();
 
-    game.on_clicked(|index: i32| {
-        std::println!("Number {}", index);
+    game.set_pieces(pieces.clone());
+
+    game.on_clicked({
+        let game_weak = game.as_weak();
+
+        move |index: i32| {
+            let game = game_weak.unwrap();
+
+            let new_data = PieceData {
+                index,
+                ..pieces.row_data(0).unwrap()
+            };
+
+            pieces.set_row_data(0, new_data);
+        }
     });
 
     game.run()
