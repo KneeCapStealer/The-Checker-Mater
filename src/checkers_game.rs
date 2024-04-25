6b5c700d7ff69d727323a3213d8ae67bb8d6fcbc
Christoffer Hald Christensen <chrishald@duck.com>
2024-04-25 11:04:23 +0200

    Implemented default board function

--- src/checkers_game.rs
+++ src/checkers_game.rs
@@ -1,7 +1,14 @@
 use slint::Model;
 use std::rc::Rc;
 
+
 slint::include_modules!();
+
+pub struct Move {
+    start: usize,
+    end: usize,
+    captured: Option<usize>,
+}
 
 pub type Tile = Option<PieceData>;
 
@@ -25,16 +32,33 @@ impl Board {
         let pieces_vec: Vec<PieceData> = game.get_pieces().iter().collect();
         let pieces_model = Rc::new(slint::VecModel::from(pieces_vec));
 
-        // TODO: Generate default tiles array
+        let tiles = Board::default_setup(PieceColor::default());
 
         Board {
             pieces_model,
+            tiles,
             ..Default::default()
         }
     }
 
-    fn default_setup() -> [Tile; 32] {
-        todo!();
+    fn default_setup(player_color: PieceColor) -> [Tile; 32] {
+        let enemy_color = match player_color {
+            PieceColor::White => PieceColor::Black,
+            PieceColor::Black => PieceColor::White,
+        };
+
+        let mut tiles: [Tile; 32] = Default::default();
+        for (i, tile) in tiles.iter_mut().enumerate() {
+            *tile = if i < 24 {
+                Some(PieceData {color: enemy_color, index: i as i32, is_king: false})
+            } else if i < 24 + 8 {
+                None
+            } else {
+                Some(PieceData {color: player_color, index: i as i32, is_king: false})
+            }
+        }
+
+        tiles
     }
 
     pub fn start_new_game(&mut self, color: PieceColor) {
@@ -57,11 +81,7 @@ impl Board {
         todo!();
     }
 
-    pub fn get_legal_moves(&self) -> Vec<usize> {
-        todo!();
-    }
-
-    pub fn get_piece_legal_moves(&self, piece: usize) -> Vec<usize> {
+    pub fn get_legal_moves(&self) -> Vec<Move> {
         todo!();
     }
 }
